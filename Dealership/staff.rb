fbd1c80c0cd42dddc6538766dbe484155a80ab44
LupercusWhitewolf <34462063+LupercusWhitewolf@users.noreply.github.com>
2018-01-02 21:07:36 -0600

    update

--- Dealership/staff.rb
+++ Dealership/staff.rb
@@ -1,10 +1,11 @@
 class Staff
   #Formats the salesmans name
-  def initialize(last, first, current, goal)
+  def initialize(last, first, current=0, goal)
     @first = first.capitalize
     @last = last.capitalize
     @current = current
     @goal = goal
+    @staff = []
   end
   #Formats the greeting
   def name
@@ -17,11 +18,35 @@ class Staff
     self.current += 1
     puts "#{@first} just made a sale! They now have #{@current} sales."
   end
+
+  def addstaff
+    puts "\n Please enter the following information."
+    puts "\n Last Name:"
+    lname = gets.chomp.downcase
+    puts "\n First Name:"
+    fname = gets.chomp.downcase
+    puts "\n Sales Goal:"
+    sgoal = gets.chomp.to_i
+    @staff.push(new Staff(lname, fname, 0, sgoal))
+  end
+
+  def printstaff
+    @staff.each do |staff|
+      puts name
+    end
+  end
+
   attr_reader :last
   attr_reader :first
   attr_accessor :current
   attr_reader :goal
 end
+
+def
+
+addstaff
+printstaff
+
 staff1 = Staff.new("murdock", "matthew", 26, 164)
 
 if __FILE__ == $0
